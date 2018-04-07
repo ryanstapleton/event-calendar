66448be05db46e6e0e8d80a0c989fec090f50657
Jukinner <jukinner@gmail.com>
2018-04-06 20:58:54 -0600

    Major steps on API connection

--- app/controllers/events_controller.rb
+++ app/controllers/events_controller.rb
@@ -1,6 +1,6 @@
 class EventsController < ApplicationController
-  before_action :authenticate_user!, only: [:create, :new, :edit, :update, :destroy]
-  before_action :set_event, only: [:show, :edit, :update, :destroy, :approve, :reject]
+  before_action :authenticate_user!, only: [:create, :new, :edit, :update, :destroy, :redirect, :callback, :calendars, :send]
+  before_action :set_event, only: [:show, :edit, :update, :destroy, :approve, :reject, :send]
   before_action :set_event_show, only: [:edit, :update, :destroy]
   access all: [:show, :index], user: {except: [:admin]}, admin: :all
   
@@ -8,12 +8,10 @@ class EventsController < ApplicationController
     @events = Event.where(status: :approved).order(date: :desc)
     @rsvps = current_user.rsvps if current_user
     @favorites = current_user.favorites if current_user
-    @calendar_list
   end
 
   def show
     @event = Event.find(params[:id])
-    @location = "2912+Executive+Pkwy,Lehi,UT"
   end
 
 
@@ -57,11 +55,12 @@ class EventsController < ApplicationController
 
   def approve
     @event.approved!
-    respond_to do |format|
-      format.html { redirect_to admin_path, notice: 'The event was approved.' }
-    end
+    # respond_to do |format|
+    #   format.html { redirect_to admin_path, notice: 'The event was approved.' }
+    # end
 
     # TODO: API CALL - insert event into API
+    redirect_to url_for(:action => :send)
   end
 
   def reject
@@ -69,6 +68,66 @@ class EventsController < ApplicationController
     respond_to do |format|
       format.html { redirect_to admin_path, notice: 'The event was rejected.' }
     end
+  end
+
+
+  def redirect
+    client = Signet::OAuth2::Client.new(client_options)
+
+    redirect_to client.authorization_uri.to_s
+  end
+
+  def callback
+    client = Signet::OAuth2::Client.new(callback_options)
+
+    response = client.fetch_access_token!
+
+    session[:access_token] = response['access_token']
+
+    redirect_to url_for(:action => :calendars)
+  end
+
+  def calendars
+    client = Signet::OAuth2::Client.new(access_token: session[:access_token])
+
+    service = Google::Apis::CalendarV3::CalendarService.new
+
+    service.authorization = client
+    service.authorization.expires_in = Time.now + 1_000_000
+
+    # byebug
+    @calendar_list = service.list_events('primary')
+
+    redirect_to admin_path
+  
+    rescue Google::Apis::AuthorizationError
+
+    response = client.refresh!
+
+    session[:authorization] = session[:authorization].merge(response)
+
+    retry
+  end
+
+
+  def send
+    # client = Signet::OAuth2::Client.new(client_options)
+    # client.update!(session[:authorization])
+
+    # service = Google::Apis::CalendarV3::CalndarService.new
+    # service.authorization = client
+
+    today = Date.today
+
+    api_data = Google::Apis::CalendarV3::Event.new({
+      start: Google::Apis::CalendarV3::EventDateTime.new(date:today + 1),
+      end: Google::Apis::CalendarV3::EventDateTime.new(date:today + 2),
+      description: @event.body
+    })
+
+    service.insert_event(params['primary'], api_data)
+
+    redirect_to admin_path, notice: 'The event was approved'
   end
 
   private
@@ -86,5 +145,28 @@ class EventsController < ApplicationController
 
     def event_params
       params.require(:event).permit(:title, :body, :main_image, :date, :location)
+    end
+
+
+
+    def client_options
+      {
+        client_id: ENV.fetch('GOOGLE_CLIENT_ID'),
+        client_secret: ENV.fetch('GOOGLE_CLIENT_SECRET'),
+        authorization_uri: 'https://accounts.google.com/o/oauth2/auth',
+        token_credential_uri: 'https://accounts.google.com/o/oauth2/token', 
+        scope: Google::Apis::CalendarV3::AUTH_CALENDAR,
+        redirect_uri: url_for(:action => :callback)
+      }
+    end
+
+    def callback_options
+      {
+        client_id: ENV.fetch('GOOGLE_CLIENT_ID'),
+        client_secret: ENV.fetch('GOOGLE_CLIENT_SECRET'),
+        token_credential_uri: 'https://accounts.google.com/o/oauth2/token',
+        redirect_uri: url_for(:action => :callback),
+        code: params[:code]
+      }
     end
 end
--- config/routes.rb
+++ config/routes.rb
@@ -5,10 +5,10 @@ Rails.application.routes.draw do
   devise_for :users, path: '', path_names: {sign_in: 'login', sign_out: 'logout', sign_up: 'register'}
   
   # resources :api do
-    get '/redirect', to: 'api#redirect', as: 'redirect'
-    get '/callback', to: 'api#callback', as: 'callback'
-    get '/current_events', to: 'api#calendars', as: 'current_events'
-    post '/api/create_event', to: 'api#create_event', as: 'create_event', calendar_id: 'primary'
+    get '/redirect', to: 'events#redirect', as: 'redirect'
+    get '/callback', to: 'events#callback', as: 'callback'
+    get '/current_events', to: 'events#calendars', as: 'current_events'
+    post '/send', to: 'events#send', calendar_id: 'primary'
   # end
 
   
@@ -16,12 +16,12 @@ Rails.application.routes.draw do
     member do
       get :approve
       get :reject
+      # post :send, calendar_id: 'primary'
     end
   end
   
   resources :rsvps, only: [:new, :create, :destroy]
   resources :favorites, only: [:new, :create, :destroy]
-  resources :api, only: [:callback, :redirect, :calendars, :create_event ]
 
   root :to => 'events#index'
 end
